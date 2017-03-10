4c93a42681c5d1960af89673416319cfd8a75e86
Alexey V Paramonov <me@paramonov.ru>
2017-03-10 19:50:32 +0300

    Respond with token after successfull signup

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import permissions, status
 from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
 
 from users.serializers import UserSerializer
 from users.models import User
@@ -44,10 +45,22 @@ class Users(APIView):
             serializer = UserSerializer(data=request.data)
 
             if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                user = serializer.save()
 
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+                jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+
+                return Response(
+                    {"token": token},
+                    status=status.HTTP_201_CREATED
+                )
+
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
         else:
             # not implemented yet
             return Response(status=status.HTTP_400_BAD_REQUEST)

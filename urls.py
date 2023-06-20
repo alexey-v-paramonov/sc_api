bdfddeb71f87382cbb87d9a8d47a61738b66a816
Alexey V Paramonov <me@paramonov.ru>
2023-06-20 10:38:36 +0300

    Speechkit integration

--- urls.py
+++ urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/v1/api-token-auth/', SCObtainAuthToken.as_view()),
     path('api/v1/',       include('users.urls')),
     path('api/v1/',       include('radio.urls')),
+    path('api/v1/',       include('voiceover.urls')),
 ]

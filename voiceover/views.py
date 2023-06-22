78d46cff40d17d2f8f48d675558e5508fad5aa76
Alexey V Paramonov <me@paramonov.ru>
2023-06-23 01:04:04 +0300

    Use LAME in pipe mode for PCM audio encoding

--- voiceover/views.py
+++ voiceover/views.py
@@ -1,9 +1,12 @@
+import subprocess
+
 from django.conf import settings
 from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from speechkit import Session
 from speechkit import SpeechSynthesis
+
 
 
 class VoiceoverAPI(APIView):
@@ -24,20 +27,29 @@ class VoiceoverAPI(APIView):
         
         voice = request.data.get("voice", "filipp")
         speed = request.data.get("speed", "1")
+        emotion = request.data.get("emotion", "neutral")
 
         session = Session.from_api_key(settings.SPEECHKIT_API_KEY, settings.SPEECHKIT_FOLDER_ID)
         synthesizeAudio = SpeechSynthesis(session)
 
-        synthesizeAudio.synthesize(
-            'news.wav',
+        pcm_buff = synthesizeAudio.synthesize_stream(
             text=text,
             lang=lang,
             voice=voice,
             format='lpcm',
             speed=speed,
+            emotion=emotion,
             sampleRateHertz='48000'
         )
-
+        process=subprocess.Popen(
+            ["lame", 
+             "-r", 
+             "-s", "22.05", 
+             "-q", "0", 
+             "-", 
+             "-"
+             ], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout_data = process.communicate(input=pcm_buff)[0]
 
         return Response({})
 

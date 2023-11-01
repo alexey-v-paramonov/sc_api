b1d9bd18abbeab78f91e313e8e45ff4aa40dc845
Alexey V Paramonov <me@paramonov.ru>
2023-11-01 19:01:50 +0300

    Conditional SSH params check

--- radio/serializers.py
+++ radio/serializers.py
@@ -25,9 +25,9 @@ class SelfHostedRadioSerializer(CustomErrorMessagesModelSerializer):
     def validate(self, data):
 
         ip = data['ip']
-        ssh_username  = data['ssh_username']
-        ssh_password = data['ssh_password']
-        ssh_port = data['ssh_port']
+        ssh_username  = data.get('ssh_username', None)
+        ssh_password = data.get('ssh_password', None)
+        ssh_port = data.get('ssh_port', None)
         domain = data.get('domain', None)
         if domain:
 
@@ -41,33 +41,34 @@ class SelfHostedRadioSerializer(CustomErrorMessagesModelSerializer):
                 raise serializers.ValidationError({"domain": "wrong_ip"})
 
         # Check SSH port connection
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5)
+        if ssh_username and ssh_password and ssh_port:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(5)
 
-        try:
-            s.connect((ip, int(ssh_port)))
-        except Exception:
-            raise serializers.ValidationError({"ip": "ssh_port_connection_failed"})
-        finally:
-            s.shutdown(socket.SHUT_RDWR)
-            s.close()
+            try:
+                s.connect((ip, int(ssh_port)))
+            except Exception:
+                raise serializers.ValidationError({"ip": "ssh_port_connection_failed"})
+            finally:
+                s.shutdown(socket.SHUT_RDWR)
+                s.close()
 
-        # Check SSH credentials
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            print(ip, ssh_username, ssh_password, ssh_port)
-            ssh.connect(ip, username=ssh_username, password=ssh_password, port=ssh_port, allow_agent=False, look_for_keys=False)
-        except Exception:
+            # Check SSH credentials
+            try:
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                print(ip, ssh_username, ssh_password, ssh_port)
+                ssh.connect(ip, username=ssh_username, password=ssh_password, port=ssh_port, allow_agent=False, look_for_keys=False)
+            except Exception:
+                ssh.close()        
+                raise serializers.ValidationError({"ip": "ssh_connection_failed"})
+
+            _, stdout, _ = ssh.exec_command('cat /etc/system-release /etc/issue 2>/dev/null')
+            ssh_output = stdout.read().lower().decode("utf-8") 
             ssh.close()        
-            raise serializers.ValidationError({"ip": "ssh_connection_failed"})
 
-        _, stdout, _ = ssh.exec_command('cat /etc/system-release /etc/issue 2>/dev/null')
-        ssh_output = stdout.read().lower().decode("utf-8") 
-        ssh.close()        
-
-        if ssh_output.find("centos") == -1 and ssh_output.find("ubuntu") == -1:
-            raise serializers.ValidationError({"ip": "wrong_os"})
+            if ssh_output.find("centos") == -1 and ssh_output.find("ubuntu") == -1:
+                raise serializers.ValidationError({"ip": "wrong_os"})
             
         return data
 

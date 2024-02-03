45cc5cabbe89fe6f6855694f23e4ddfe00b87b96
Alexey V Paramonov <me@paramonov.ru>
2024-02-03 15:47:41 +0300

    Mobile app radio models, views, serializers

--- mobile_application/serializers.py
+++ mobile_application/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
-from mobile_application.models import AndroidApplication, IosApplication
+from mobile_application.models import AndroidApplication, IosApplication, AndroidApplicationRadio, IosApplicationRadio
 
+from util.serializers import (
+    CustomErrorMessagesModelSerializer,
+)
 
-class AndroidApplicationSerializer(serializers.ModelSerializer):
+class AndroidApplicationSerializer(CustomErrorMessagesModelSerializer):
+
+    class Meta:
+        model = AndroidApplicationRadio
+        exclude = ()
+
+class IosApplicationRadioSerializer(CustomErrorMessagesModelSerializer):
+
+    class Meta:
+        model = IosApplicationRadio
+        exclude = ()
+
+class AndroidApplicationRadioSerializer(CustomErrorMessagesModelSerializer):
 
     class Meta:
         model = AndroidApplication
         exclude = ()
 
-class IosApplicationSerializer(serializers.ModelSerializer):
+class IosApplicationSerializer(CustomErrorMessagesModelSerializer):
 
     class Meta:
         model = IosApplication
--- mobile_application/views.py
+++ mobile_application/views.py
@@ -5,8 +5,8 @@ from rest_framework import (
     permissions,
     routers
 )
-from mobile_application.serializers import AndroidApplicationSerializer, IosApplicationSerializer
-from mobile_application.models import AndroidApplication, IosApplication
+from mobile_application.serializers import AndroidApplicationSerializer, IosApplicationSerializer, AndroidApplicationRadioSerializer, IosApplicationRadioSerializer
+from mobile_application.models import AndroidApplication, IosApplication, AndroidApplicationRadio, IosApplicationRadio
 
 class AndroidApplicationViewSet(viewsets.ModelViewSet):
     permission_classes = [
@@ -24,8 +24,29 @@ class IosApplicationViewSet(viewsets.ModelViewSet):
     serializer_class = IosApplicationSerializer
     queryset = IosApplication.objects.all()
 
+
+class AndroidApplicationRadioViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = AndroidApplicationRadioSerializer
+    queryset = AndroidApplicationRadio.objects.all()
+
+
+class IosApplicationRadioViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = IosApplicationRadioSerializer
+    queryset = IosApplicationRadio.objects.all()
+
 android_app_router = routers.SimpleRouter()
 ios_app_router = routers.SimpleRouter()
+
+android_radio_router = routers.SimpleRouter()
+ios_radio_router = routers.SimpleRouter()
 
 android_app_router.register(
     r'android',
@@ -37,4 +58,16 @@ ios_app_router.register(
     r'ios',
     IosApplicationViewSet,
     basename='ios_app'
+)
+
+android_radio_router.register(
+    r'android_radio',
+    AndroidApplicationRadioViewSet,
+    basename='android_radio'
+)
+
+ios_radio_router.register(
+    r'ios_radio',
+    IosApplicationRadioViewSet,
+    basename='ios_radio'
 )

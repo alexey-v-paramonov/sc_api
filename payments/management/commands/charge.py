c8d78b5de6c1c5407c471dba1bece3120db8e79d
Alexey V Paramonov <me@paramonov.ru>
2024-02-25 23:00:10 +0300

    Add extra disk usage charges

--- payments/management/commands/charge.py
+++ payments/management/commands/charge.py
@@ -3,13 +3,14 @@ import datetime
 from django.core.management.base import BaseCommand, CommandError
 from django.template.loader import get_template
 from decimal import Decimal
+from users.models import Currency
 
 from users.models import User
 from payments.models import Charge, ChargedServiceType
 from django.utils.html import strip_tags
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
-from radiotochka.billing import PRICE_PER_EXTRA_GB
+from radiotochka.billing import PRICE_PER_EXTRA_GB, PRICE_PER_EXTRA_GB_USD
 
 class Command(BaseCommand):
     help = "Charge users daily"
@@ -59,7 +60,8 @@ class Command(BaseCommand):
                     disk_quota_mb = disk_quota * 1024.
                     above_allowed_du = hosted_radio.disk_usage - disk_quota_mb
                     if above_allowed_du > 0:
-                        price_du_day = PRICE_PER_EXTRA_GB / n_month_days * (above_allowed_du / 1024.)
+                        du_price = PRICE_PER_EXTRA_GB_USD if user.is_usd() else PRICE_PER_EXTRA_GB
+                        price_du_day = du_price / n_month_days * (above_allowed_du / 1024.)
                         total_daily += Decimal(price_du_day)
                         Charge.objects.create(
                             user=user,

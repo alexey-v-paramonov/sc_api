8d573e20c84d41cc1e705ccd8faf99296725be0e
Alexey V Paramonov <me@paramonov.ru>
2024-03-17 10:58:03 +0300

    Extend admin user model

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from users.models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_per_page = 1500
+    search_fields = ['email', ]
+
 admin.site.register(User)
